8790316342ea5d434b151a385c9d10499189e0a9
Aldric Giacomoni <trevoke@gmail.com>
2012-12-29 18:53:39 -0500

    Add Cabinet::File

--- lib/cabinet/file.rb
+++ lib/cabinet/file.rb
@@ -2,9 +2,21 @@ require 'fileutils'
 
 module Cabinet
   class File
-    def self.add x
-      FileUtils.touch x 
-      Secretary.organize x
+    def self.add file
+      FileUtils.touch file 
+      Secretary.organize file
+    end
+
+    def self.retrieve filename
+      new filename
+    end
+
+    def initialize file
+      @file = file
+    end
+
+    def name
+      @file
     end
   end
 end
--- test/file_test.rb
+++ test/file_test.rb
@@ -2,19 +2,20 @@ require_relative "../lib/cabinet.rb"
 require 'minitest/autorun'
 
 module Cabinet
-  class FileTest < MiniTest::Spec
+  describe File do
     let(:file) { "filename" }
-    describe "Creating files" do
-      after do
-        FileUtils.rm_f file
-      end
-      it "is as simple as putting a file in a drawer" do
-        File.add file
-        ::File.exists?(file).must_be :==, true
-      end
-      it "adds them to the list of managed files" do
-        File.add file
-        Secretary.known_files.must_include file
+    after { FileUtils.rm_f file }
+
+    it "can be added" do
+      File.add file
+      ::File.exists?(file).must_equal true
+    end
+
+    describe "info" do
+      before { File.add file }
+      subject { File.retrieve file }
+      it "has the name" do
+        subject.name.must_equal file
       end
     end
   end
